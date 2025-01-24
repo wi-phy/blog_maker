6d809605a23c57081d071ddd28a77feefc73f6d2
Clément Demongeot <clement.demongeot.art@gmail.com>
2025-01-24 16:31:51 +0100

    feat: add homePage routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './core/pages/home/home.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomePage },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
