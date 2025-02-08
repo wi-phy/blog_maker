a1e092796ebb62a49857ee3c00dcffd844c4d813
wiphy <lm802085@gmail.com>
2025-02-08 19:25:01 +0100

    feat: add spartan brain package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require("@spartan-ng/brain/hlm-tailwind-preset")],
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
