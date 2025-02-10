fc03fc8d5cf07675a7d4b5f70fe1b6f6ccaaf728
wiphy <lm802085@gmail.com>
2025-02-10 18:47:06 +0100

    feat: add signals / eyes tout ça tout ça

--- src/app/core/pages/home/home.page.ts
+++ src/app/core/pages/home/home.page.ts
@@ -1,13 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { lucideEye, lucideEyeOff } from '@ng-icons/lucide';
-import { provideIcons, NgIconComponent } from '@ng-icons/core';
-import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
 import { CardComponent } from '../../../features/article/component/card/card.component';
 
 @Component({
-  imports: [CardComponent, HlmIconDirective, NgIconComponent],
-  providers: [provideIcons({ lucideEye, lucideEyeOff })],
+  imports: [CardComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })
