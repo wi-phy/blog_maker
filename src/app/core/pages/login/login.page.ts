16f5de01eb16f16fc4c934b427a061a9890e80c7
Clément Demongeot <clement.demongeot.art@gmail.com>
2025-02-12 17:48:27 +0100

    add : show / hide eye for password

--- src/app/core/pages/login/login.page.ts
+++ src/app/core/pages/login/login.page.ts
@@ -1,15 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { lucideEye, lucideEyeOff } from '@ng-icons/lucide';
+import { provideIcons, NgIconComponent } from '@ng-icons/core';
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [HlmIconDirective, NgIconComponent],
+  providers: [provideIcons({ lucideEye, lucideEyeOff })],
   templateUrl: './login.page.html',
   styleUrl: './login.page.scss',
 })
 export class LoginPage {
   router = inject(Router);
 
+  isPasswordShown = signal(false);
+
   goToHome() {
     this.router.navigate(['/home']);
+  }
+
+  hidePassword() {
+    this.isPasswordShown.set(false);
+  }
+
+  showPassword() {
+    this.isPasswordShown.set(true);
   }
 }
--- src/app/core/pages/signup/signup.page.ts
+++ src/app/core/pages/signup/signup.page.ts
@@ -1,16 +1,37 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { lucideEye, lucideEyeOff } from '@ng-icons/lucide';
+import { provideIcons, NgIconComponent } from '@ng-icons/core';
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [HlmIconDirective, NgIconComponent],
+  providers: [provideIcons({ lucideEye, lucideEyeOff })],
   templateUrl: './signup.page.html',
   styleUrl: './signup.page.scss',
 })
 export class SignupPage {
   router = inject(Router);
 
+  isPasswordShown = signal(false);
+  isConfirmPasswordShown = signal(false);
+
   goToHome() {
     this.router.navigate(['/home']);
+  }
+  showPassword() {
+    this.isPasswordShown.set(true);
+  }
+
+  hidePassword() {
+    this.isPasswordShown.set(false);
+  }
+
+  showConfirmPassword() {
+    this.isConfirmPasswordShown.set(true);
+  }
+
+  hideConfirmPassword() {
+    this.isConfirmPasswordShown.set(false);
   }
 }
